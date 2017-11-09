62295d58811f81b071693cba345c12706a271a68
caravinden <caravinden@hotmail.com>
2017-11-09 16:52:46 +0530

    Method chaining example

---
+++ src/js/NumOperation.js
@@ -0,0 +1,35 @@
+/**
+*	Method helps to demonstrate the usage of
+*	method chaining.
+*
+*/
+var NumOperation = function (valArr) {
+	this.valArr = valArr;
+	
+	// sort the array
+	this.sort = function () {
+		this.valArr.sort(function (itemA, itemB) {
+			return itemA - itemB;
+		});
+		return this;
+	};
+	
+	// To find the sum of whole the numbers
+	this.sum = function () {
+		var me = this;
+		me.sumVal = 0;
+		me.valArr.forEach(function (d) {
+			me.sumVal = me.sumVal + d;
+		});
+		return me;
+	};
+	
+	// To find the mean value of the input array
+	this.mean = function () {
+		var me = this;
+		me.sum();
+		me.meanVal = (me.sumVal / me.valArr.length);
+		return this;
+	};
+
+};
